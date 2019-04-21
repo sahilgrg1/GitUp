5608571d98f8758461166630182d8cdd5fde8590
sahilgrg1 <gargsahil898@gmail.com>
2019-04-21 20:09:04 +0530

    My App

--- app/src/main/java/com/example/gitup/SplashActivity.kt
+++ app/src/main/java/com/example/gitup/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gitup
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,6 +9,8 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        var intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
